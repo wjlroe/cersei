402eb6a3e6c7bb850258a6990cb519f45c3f9645
William Roe <willroe@gmail.com>
2012-06-24 22:53:22 +0100

    Format json message for the websocket nicely

--- src/group_stats.erl
+++ src/group_stats.erl
@@ -1,5 +1,14 @@
 -module(group_stats).
 -export([update_stats/5]).
 
-update_stats(_Group, _Project, _BuildNumber, _Result, _BuildOutcome) ->
-    ok.
+update_stats(Group, Project, BuildNumber, Result, {BuildOutcome, BuildResults}) ->
+    Msg = mochijson2:encode({struct, [
+                                      {group, list_to_binary(Group)},
+                                      {project, list_to_binary(Project)},
+                                      {build_number, BuildNumber},
+                                      {result, Result},
+                                      {build_outcome, BuildOutcome},
+                                      {build_results,
+                                       {struct, BuildResults}}
+                                     ]}),
+    build_group_websocket_handler:send_message(Msg).

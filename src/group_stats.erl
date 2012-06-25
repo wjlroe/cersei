e342568200c168be4776da736159c0d4f0e76a22
William Roe <willroe@gmail.com>
2012-06-26 00:04:15 +0100

    Function to send a random build message on web socket

--- src/group_stats.erl
+++ src/group_stats.erl
@@ -1,5 +1,6 @@
 -module(group_stats).
 -export([update_stats/5]).
+-export([send_random_message/0]).
 
 update_stats(Group, Project, BuildNumber, Result, {BuildOutcome, BuildResults}) ->
     Msg = mochijson2:encode({struct, [
@@ -12,3 +13,17 @@ update_stats(Group, Project, BuildNumber, Result, {BuildOutcome, BuildResults}) 
                                        {struct, BuildResults}}
                                      ]}),
     build_group_websocket_handler:send_message(Msg).
+
+send_random_message() ->
+    RandBuildNum = random:uniform(120),
+    RandBugNum   = random:uniform(10) - 1,
+    RandTestNum  = random:uniform(50),
+    Result = io_lib:format("There were ~B bugs with a total of ~B tests.", [RandBugNum, RandTestNum]),
+    BuildOutcome = case RandBugNum of
+                       0 ->
+                           pass;
+                       _ ->
+                           fail
+                   end,
+    BuildResults = {BuildOutcome, [{bugs, RandBugNum}, {tests, RandTestNum}]},
+    update_stats("blah", "blah-frontend", RandBuildNum, list_to_binary(Result), BuildResults).

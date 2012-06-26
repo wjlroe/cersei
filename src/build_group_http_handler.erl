046135f8f3233711c0637f37a81a68f580628214
William Roe <willroe@gmail.com>
2012-06-26 10:42:01 +0100

    Default index.html for /

--- src/build_group_http_handler.erl
+++ src/build_group_http_handler.erl
@@ -14,8 +14,9 @@ handle(Req, State) ->
                    "/" ++ NotAbsolute -> NotAbsolute;
                    _ -> Path
                end,
-    io:format("Filename: ~p~n", [Filename]),
-    FileLocation = filename:join(["./public", Filename]),
+    RewrittenFilename = rewrite_path(Filename),
+    io:format("Filename: ~p~n", [RewrittenFilename]),
+    FileLocation = filename:join(["./public", RewrittenFilename]),
     io:format("FileLocation: ~p~n", [FileLocation]),
     {ok, Req2} = case filelib:is_regular(FileLocation) of
                      true ->
@@ -25,6 +26,14 @@ handle(Req, State) ->
                          cowboy_http_req:reply(404, [], <<"No such file">>, Req)
                  end,
     {ok, Req2, State}.
+
+rewrite_path(Filename) ->
+    case Filename of
+        "" ->
+            "index.html";
+        _ ->
+            Filename
+    end.
 
 terminate(_Req, _State) ->
     ok.
